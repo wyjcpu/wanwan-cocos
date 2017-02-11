3884d196812e81e569584c89429be7f1702adf2c
[wanwang2] <1053400545@qq.com>
2017-02-11 22:25:43 +0800

    vv

--- src/main/java/org/wanwanframework/coco/core/Instruction.java
+++ src/main/java/org/wanwanframework/coco/core/Instruction.java
@@ -1,5 +1,10 @@
 package org.wanwanframework.coco.core;
 
+import java.util.Map;
+
+import org.wanwanframework.file.map.LineTool;
+import org.wanwanframwork.file.Log;
+
 /**
  * 原子指令
  * @author coco
@@ -7,4 +12,28 @@ package org.wanwanframework.coco.core;
  */
 public class Instruction {
 
+	private Map<String, String>[] configs;
+	
+	public Instruction() {
+		Map<String, String> url = LineTool.getLine("./src/main/resources/instruction/config.txt", ":\t");
+		configs = LineTool.getConfigs(url.get("url"), ":\t");
+	}
+	
+	public void init() {
+		for(int i = 0; i < configs.length; i++) {
+			readConfig(configs[i]);
+		}
+	}
+	
+	private void readConfig(Map<String, String> config) {
+		String path = config.get("math");
+		Map<String, String> mathMap = LineTool.getLine(path, ":\r\n");
+		Log.log(mathMap);
+	}
+	
+	public static void main(String[] args) {
+		Instruction instruction = new Instruction();
+		instruction.init();
+		Log.log("instruction...");
+	}
 }
