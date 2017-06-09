fd7e45fb3724f275a7dbcd0b413cee7c099adc4a
wanwang2 <1053400545@qq.com>
2017-06-09 17:30:13 +0800

    ...add loopController

--- wanwan-coco/src/main/java/org/wanwanframework/coco/lab/excel/ExcelReadLoopController.java
+++ wanwan-coco/src/main/java/org/wanwanframework/coco/lab/excel/ExcelReadLoopController.java
@@ -1,5 +1,37 @@
 package org.wanwanframework.coco.lab.excel;
 
-public class ExcelReadLoopController {
+import java.io.File;
+import java.util.Arrays;
 
+import org.wanwanframework.file.config.ConfigController;
+import org.wanwanframwork.file.Log;
+import org.wanwanframwork.file.core.FileController;
+
+/**
+ * 循环调用器
+ * @author coco
+ *
+ */
+public class ExcelReadLoopController extends FileController<File[]>{
+
+	protected String dirPath;
+	protected ConfigController configController = new ConfigController();
+	
+	public ExcelReadLoopController() {
+		this.dirPath = configController.getCore().get("loopPath").trim();
+		File dir = new File(dirPath);
+		core = dir.listFiles(file -> file.getName().endsWith("xlsx") ? true: false);
+		configController.init();
+	}
+	
+	@Override
+	protected void process() {
+		Arrays.asList(core).stream().forEach(file ->{
+			Log.log("....file:" + file.getName());
+		});
+	}
+	
+	public static void main(String[] args) {
+		ExcelReadController.call(new ExcelReadLoopController());
+	}
 }
