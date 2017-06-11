9faff20cb12751e36e22f674a63102ed2a6f7baf
wanwang2 <1053400545@qq.com>
2017-06-11 21:16:16 +0800

    ...

--- wanwan-coco/src/main/java/org/wanwanframework/coco/lab/move/MoveController.java
+++ wanwan-coco/src/main/java/org/wanwanframework/coco/lab/move/MoveController.java
@@ -1,10 +1,12 @@
 package org.wanwanframework.coco.lab.move;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.wanwanframwork.file.FileUtil;
 import org.wanwanframwork.file.core.FileController;
 
 public class MoveController extends FileController<List<File>>{
@@ -19,6 +21,7 @@ public class MoveController extends FileController<List<File>>{
 	protected void process() {
 		core = getFile(list, new File("./src/main/resources/config"));
 		content = core.toString();
+		writeFile("./src/main/resources/config/lab/test", core);
 	}
 	
 	protected List<File> getFile(List<File> list, File dirPath) {
@@ -29,6 +32,16 @@ public class MoveController extends FileController<List<File>>{
 			getFile(list, file);
 		});
 		return list;
+	}
+	
+	protected void writeFile(String dir, List<File> files) {
+		files.stream().forEach(file ->{
+			try {
+				FileUtil.copy(file.getPath(), dir + "/" + file.getName());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 	
 	public static void main(String[] args) {
