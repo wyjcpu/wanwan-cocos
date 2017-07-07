4d19ee82a8b9c3745324c6b037c0ef9e04abd095
wanwang2 <1053400545@qq.com>
2017-07-07 20:17:41 +0800

    ..

--- wanwan-coco-partition/src/main/java/org/wanwanframework/coco/partition/controller/PartitionController.java
+++ wanwan-coco-partition/src/main/java/org/wanwanframework/coco/partition/controller/PartitionController.java
@@ -1,16 +1,22 @@
 package org.wanwanframework.coco.partition.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.wanwanframework.coco.partition.persistent.DataIdDao;
 
 @Controller
 public class PartitionController {
 
+	@Resource
+	private DataIdDao dao;
+	
     @RequestMapping(value="/",method=RequestMethod.GET)
     @ResponseBody
 	public String getId() {
-		return "hehe...";
+		return "getId:" + dao.getDataId();
 	}
 }
