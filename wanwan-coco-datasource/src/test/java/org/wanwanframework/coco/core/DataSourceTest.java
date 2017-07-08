9be8d3f14e1cd7f5aa0e8b6db55e8306cff9bb9f
wanwang2 <1053400545@qq.com>
2017-07-08 20:57:27 +0800

    ...

--- wanwan-coco-datasource/src/test/java/org/wanwanframework/coco/core/DataSourceTest.java
+++ wanwan-coco-datasource/src/test/java/org/wanwanframework/coco/core/DataSourceTest.java
@@ -2,11 +2,14 @@ package org.wanwanframework.coco.core;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Date;
+
 import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -17,9 +20,20 @@ public class DataSourceTest {
 	@Autowired
 	private DataSource dataSource;
 	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
 	@Test
 	public void testDataSource() {
 		assertTrue(dataSource != null);
 		System.out.println("druidDataSource:" + dataSource);
+	}
+	
+	@Test
+	public void testJdbcTemplate() {
+		assertTrue(jdbcTemplate != null);
+		System.out.println("jdbcTemplate:" + jdbcTemplate);
+		Date r = jdbcTemplate.queryForObject("select sysdate from dual", Date.class);
+		System.out.println(r);
 	}
 }
